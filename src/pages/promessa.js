aaac1db31ed444ee3d3d066499edbc607bf5172f
JoaoNathanIf <119938590+JoaoNathanIf@users.noreply.github.com>
2024-10-28 15:31:58 +0000

    aula dia 28-10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -12,7 +12,7 @@ export default function Promessa(){
     return <>
     <Menu/>
     <Container>
-        Promessa {prometido=== "" ?"<carregando>": prometido}
+        Promessa {prometido=== "" ? <img src="./loading.gif" width={100} height={50}/> : prometido}
     </Container>
     <Footer/>
     </>
